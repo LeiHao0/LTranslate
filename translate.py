123281be51eab896944663a5614ad82679bb1f5e
leihao <leihao.sg@gmail.com>
2020-12-13 18:38:04 +0800

    Update

--- translate.py
+++ translate.py
@@ -1,17 +1,20 @@
+import getopt
+import sys
 import os
 from google_trans_new import google_translator
-
 # https://github.com/lushan88a/google_trans_new
+
+# translate.py filename lang_src lang_tgt
+
+# lang:
+
+# https://cloud.google.com/translate/docs/languages
 
 translator = google_translator()
 
-filename = 'test1.md'
-lang_src = 'zh-cn'
-lang_tgt = 'en'
-
 
 def translate(text):
-    return translator.translate(text)
+    return translator.translate(text, lang_src=lang_src, lang_tgt=lang_tgt)
 
 
 def markdown(lines):
@@ -23,11 +26,13 @@ def markdown(lines):
         leadingSpaces = len(line) - len(l)
         if l.strip() == '<!--more-->' or not l.strip():
             transLine = l
-        elif l.startswith(('```', '$$')):
+        elif l.startswith(('```', '{% ', '<details>')):
             skip = not skip
             transLine = l
         elif l.startswith('- '):
             transLine = '- ' + translate(l[1:])
+        elif l.startswith('$$'):
+            transLine = l
         else:
             if skip:
                 transLine = l
@@ -51,9 +56,11 @@ def trans2File(transText):
     f.close()
 
 
-def test():
-    transText = markdown(file2Lines())
-    trans2File(transText)
+def main(argv):
+    global filename, lang_src, lang_tgt
+    filename, lang_src, lang_tgt = argv
+    trans2File(markdown(file2Lines()))
 
 
-test()
+if __name__ == "__main__":
+    main(sys.argv[1:])
